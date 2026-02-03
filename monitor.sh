ece3236595dd2486a1cd7106146956fa4b30732a
TNMY1110 <searain1110@gmail.com>
2026-02-03 17:58:07 +0900

    도전 과제

--- monitor.sh
+++ monitor.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # 시스템 모니터링 스크립트
+LOG_FILE=~/projects/cron_lab/logs/system_$(date +%Y%m%d).log
 # 현재 시간
-echo "=====$(date)====="
+echo "=====$(date)=====">> $LOG_FILE
 # 디스크 사용량 (/파티션만)
-echo "[디스크]"
-df -h | tail -1
+echo "[디스크]" >> $LOG_FILE
+df -h | tail -1 >> $LOG_FILE
 # 메모리 사용량
-echo "[메모리]"
-free -h | grep Mem
-echo ""
+echo "[메모리]" >> $LOG_FILE
+free -h | grep Mem >> $LOG_FILE
+echo "" >> $LOG_FILE
